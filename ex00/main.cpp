5a4ae3781fe124c28d8506c892451946f4304421
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-03-02 14:39:46 -0300

    starting ex01

---
+++ ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 14:34:06 by hde-camp          #+#    #+#             */
+/*   Updated: 2023/03/02 14:35:07 by hde-camp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,15 +6,17 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 14:09:56 by hde-camp          #+#    #+#             */
-/*   Updated: 2023/03/02 14:16:27 by hde-camp         ###   ########.fr       */
+/*   Updated: 2023/03/02 14:37:20 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 template<typename T>
 void swap(T& first, T& second){
 	T temp = first;
 	first = second;
-	second = first;
+	second = temp;
 }
 
 template<typename T>
